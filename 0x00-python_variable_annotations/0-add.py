1f25e9730c5cd4b7c6e555caa9a58db43f6c9357
Soukaina Lachheb <soukainalachheb93@gmail.com>
2024-01-11 21:06:33 +0100

    Python - Variable Annotations Tasks

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/en python3
+''' Takes a float a and b as args and returns the sum as float'''
+
+def add(a: float, b: float) -> float:
+    '''Defining The function add'''
+    return a+b
