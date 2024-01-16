e1c8c6a61e8f0fa9d89d457a03dd406799e05a8e
Soukaina Lachheb <soukainalachheb93@gmail.com>
2024-01-16 21:25:46 +0100

    Async Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,11 +1,11 @@
-#!/usr//bin/env python3
+#!/usr/bin/env python3
 '''Async Generator that takes no arguments'''
 import asyncio
 import random
-import typing
+from typing import Generator
 
 
-async def async_generator() -> typing.Generator[float, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     '''Define async_generator'''
     for _ in range(10):
         await asyncio.sleep(1)
